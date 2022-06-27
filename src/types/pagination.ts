99521f801319bbee5e0cbf358627d891b285d741
Eric Campbell <ericcampbell59@gmail.com>
2022-06-27 09:36:03 -0500

    1.0.4

--- src/types/pagination.ts
+++ src/types/pagination.ts
@@ -1,38 +1,38 @@
 export interface OrderArgs {
-    field?: string;
-    direction?: OrderDirection;
+  field?: string;
+  direction?: OrderDirection;
 }
 
 export type OrderDirection = 'desc' | 'asc';
 
 export interface APIPageInfo {
-    hasNextPage: boolean;
-    hasPreviousPage: boolean;
-    startCursor?: string;
-    endCursor?: string;
+  hasNextPage: boolean;
+  hasPreviousPage: boolean;
+  startCursor?: string;
+  endCursor?: string;
 }
 
 export interface PaginatedResponse<T> {
-    edges: T[];
-    totalCount: number;
-    pageInfo: APIPageInfo;
+  edges: T[];
+  totalCount: number;
+  pageInfo: APIPageInfo;
 }
 
 export interface APINode<T> {
-    cursor: string;
-    node: T;
+  cursor: string;
+  node: T;
 }
 
 export interface APIOrderByArgs {
-    orderBy?: string;
-    orderDirection?: string;
+  orderBy?: string;
+  orderDirection?: string;
 }
 
 export interface APIPaginationArgs {
-    skip?: number;
-    take?: number;
-    after?: string;
-    before?: string;
-    first?: number;
-    last?: number;
+  skip?: number;
+  take?: number;
+  after?: string;
+  before?: string;
+  first?: number;
+  last?: number;
 }
